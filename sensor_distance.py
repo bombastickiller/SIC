e779b9be683c50c641209776cec736738e6922d6
Bryan Munoz <brymuma@gmail.com>
2023-11-25 21:17:29 -0600

    sensor2.0

--- sensor_distance.py
+++ sensor_distance.py
@@ -2,14 +2,29 @@ from gpiozero import LED, DistanceSensor
 import time
 
 sensor = DistanceSensor(echo = 14, trigger = 15)
-led = LED(18)
+verde = LED(13)
+amarillo = LED(19)
+rojo = LED(26)
+verde.off()
+amarillo.off()
+rojo.off()
 while True:
-	distance = sensor.distance * 100
-	print ("Distancia: ", distance)
-
-	if distance < 20:
-		led.on()
-	else:
-		led.off
-
-	time.sleep(0.5)
+    distance = sensor.distance * 100
+    print ("Distancia: ", distance)
+    
+    if distance < 20:
+        amarillo.off()
+        verde.off()
+        rojo.on()
+    
+    elif distance > 80:
+        amarillo.off()
+        rojo.off()
+        verde.on()
+        
+    elif distance > 20 and distance < 80:
+        verde.off()
+        rojo.off()
+        amarillo.on()
+        
+    time.sleep(0.5)
